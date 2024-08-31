07fbc7469bfa9305af9a526159f30811faf19038
Frank92613 <frankyen0613@gmail.com>
2024-08-31 22:54:12 +0800

    Frank 240831 Route to the front door&Audio

--- Assets/Scripts/NarrationManager.cs
+++ Assets/Scripts/NarrationManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,57 +9,64 @@ public class NarrationGroup
     public AudioClip[] audioClips;
     public UnityEvent[] unityEvents;
 }
+
 public class NarrationManager : MonoBehaviour
 {
-    public NarrationGroup[] narrationGroups;
-
-
+    [Header("Narration Settings")]
+    [SerializeField] private NarrationGroup[] narrationGroups;
     private AudioSource audioSource;
-
+    private Coroutine currentNarrationCoroutine; // Reference to the current narration coroutine
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         Narration_Before_SprayGame();
-
     }
+
+    #region  Every Narrations
     public void Narration_Before_SprayGame()
     {
         PlayNarration(0, 1, 1);
     }
+
     public void Narration_After_SprayGame()
     {
         PlayNarration(1, 1, 1);
     }
+
     public void Narration_When_MatchGame()
     {
         PlayNarration(2, 1, 1);
     }
 
+    public void Narration_After_MatchGame()
+    {
+        PlayNarration(3, 0, 2);
+    }
+    #endregion
 
+    #region  Audio Settings
     public void PlayNarration(int groupIndex, float start_delay, float every_audio_delay)
     {
         if (groupIndex >= 0 && groupIndex < narrationGroups.Length)
         {
             NarrationGroup group = narrationGroups[groupIndex];
-            StartNarration(group.audioClips, group.unityEvents, start_delay, every_audio_delay);
+
+            // Stop any currently playing narration
+            StopNarration();
+
+            // Start the new narration
+            currentNarrationCoroutine = StartCoroutine(PlayAudioClips(group.audioClips, group.unityEvents, start_delay, every_audio_delay));
         }
     }
 
-    public void StartNarration(AudioClip[] audioClips, UnityEvent[] events, float start_delay, float every_audio_delay)
-    {
 
-        if (audioClips.Length > 0 && events.Length > 0)
-        {
-            StartCoroutine(PlayAudioClips(audioClips, events, start_delay, every_audio_delay));
-        }
-
-    }
 
     IEnumerator PlayAudioClips(AudioClip[] audioClips, UnityEvent[] events, float start_duration, float every_audio_duration)
     {
         int currentClipIndex = 0;
         yield return new WaitForSeconds(start_duration);
+
         while (currentClipIndex < audioClips.Length)
         {
             audioSource.clip = audioClips[currentClipIndex];
@@ -69,14 +75,30 @@ public class NarrationManager : MonoBehaviour
 
             yield return new WaitForSeconds(audioSource.clip.length);
 
-
             if (currentClipIndex < events.Length && events[currentClipIndex] != null)
             {
-                //Debug.Log(currentClipIndex);
                 events[currentClipIndex].Invoke();
             }
 
             currentClipIndex++;
         }
+
+        // Reset the coroutine reference when narration is complete
+        currentNarrationCoroutine = null;
     }
+    public void StopNarration()
+    {
+        if (currentNarrationCoroutine != null)
+        {
+            StopCoroutine(currentNarrationCoroutine);
+            currentNarrationCoroutine = null;
+        }
+
+        // Stop the currently playing audio
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/VR/GameFlowManager.cs
+++ Assets/Scripts/VR/GameFlowManager.cs
@@ -15,73 +15,163 @@ public class GameFlowManager : MonoBehaviour
     [Header("Player Route Control")]
     [SerializeField] private NavMeshAgent player_agent;
     [SerializeField] List<Transform> PhotoRoute_Waypoints;
+    [SerializeField] List<Transform> DoorRoute_Waypoints;
     [SerializeField] private float RotationDuration;
 
-    private int PhotoRoute_WaypointIndex = 0;
+    [Header("GameObject Control")]
+    [SerializeField] GameObject Neighbor_OBJ;
+    [SerializeField] GameObject Daughter_OBJ;
+
+    private Coroutine moveCoroutine;
 
     // Event
     public EventHandler OnStart_PhotoRoute;
+    public EventHandler OnStart_DoorRoute;
+
+    //GetComponent
+
+    private NarrationManager narro;
 
 
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
     private void Awake()
     {
+        //Singleton
         if (Instance == null)
         {
             Instance = this;
         }
+        //Component
+        narro = GetComponent<NarrationManager>();
+
+        //Whether auto facing the destination
         player_agent.updateRotation = false;
+
+        //Fade in when starting
         fadeScreen.FadeIn();
+
+        //Events Initializations
         OnStart_PhotoRoute += Handle_OnStartPhotoRoute;
+        OnStart_DoorRoute += Handle_OnStartDoorRoute;
+        //Characters Initializations
+        DisplayNeighbor(false);
+        DisplayDaughter(false);
 
     }
-    #region AfterSprayGame
+    #region Character Display
+    public void DisplayNeighbor(bool visible)
+    {
+        Neighbor_OBJ.SetActive(visible);
+    }
+    public void DisplayDaughter(bool visible)
+    {
+        Daughter_OBJ.SetActive(visible);
+    }
+    #endregion
+
+    #region AfterSprayGames
     public void After_SprayGame()
     {
-        StartCoroutine(Route_After_SprayGame());
+        StartCoroutine(Route_After_SprayGame(180));
     }
-    IEnumerator Route_After_SprayGame()
+
+    IEnumerator Route_After_SprayGame(float RotationY)
     {
         // Hide_SprayBottle_UI
         yield return new WaitForSeconds(1f);
-        GetComponent<SprayGameManager>().Hide_SprayBottle_UI();
+        GetComponent<SprayGameManager>().Display_SprayBottle_UI(false);
         yield return new WaitForSeconds(1f);
 
-        // Start smooth rotation
-        Quaternion startRotation = player_agent.gameObject.transform.rotation;
-        Quaternion endRotation = startRotation * Quaternion.Euler(0, 180, 0);
+
+        // Start moving & narration
+        OnStart_PhotoRoute?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Handle_OnStartPhotoRoute(object sender, EventArgs e)
+    {
+
+        narro.Narration_After_SprayGame();
+        StartMoving(PhotoRoute_Waypoints);
+    }
+    #endregion
+
+    #region AfterMatchGames
+    public void After_MatchGame()
+    {
+        StartCoroutine(Route_After_MatchGame(-150));
+    }
+    IEnumerator Route_After_MatchGame(float RotationY)
+    {
+        yield return new WaitForSeconds(1f);
+        OnStart_DoorRoute?.Invoke(this, EventArgs.Empty);
+    }
+    private void Handle_OnStartDoorRoute(object sender, EventArgs e)
+    {
+
+        narro.Narration_After_MatchGame();
+        StartMoving(DoorRoute_Waypoints);
+    }
+
+    #endregion
+
+    #region Moving & Rotation
+
+    void StartMoving(List<Transform> waypoints)
+    {
+        if (moveCoroutine != null)
+        {
+            StopCoroutine(moveCoroutine);
+        }
+        moveCoroutine = StartCoroutine(MoveThroughWaypoints(waypoints));
+    }
+    IEnumerator MoveThroughWaypoints(List<Transform> waypoints)
+    {
+        int index = 0;
+        while (index < waypoints.Count)
+        {
+            // Calculate the direction to the next waypoint
+            Vector3 directionToNextWaypoint = waypoints[index].position - player_agent.transform.position;
+
+            // Calculate the target rotation based on the direction * offset
+            Quaternion targetRotation = Quaternion.LookRotation(directionToNextWaypoint) * Quaternion.Euler(0, 180, 0); ;
+
+            // Smoothly rotate the player to face the next waypoint
+            yield return StartCoroutine(SmoothRotatePlayer(targetRotation));
+
+            // Set the destination to the next waypoint
+            player_agent.SetDestination(waypoints[index].position);
+
+            // Wait until the agent reaches the destination
+            while (!player_agent.pathPending && player_agent.remainingDistance > 0.5f)
+            {
+                yield return null;
+            }
+            while (player_agent.pathPending || player_agent.remainingDistance > 0.1f)
+            {
+                yield return null;
+            }
+
+            index++;
+        }
+    }
+
+    IEnumerator SmoothRotatePlayer(Quaternion targetRotation)
+    {
+        Quaternion startRotation = player_agent.transform.rotation;
         float elapsedTime = 0f;
 
         while (elapsedTime < RotationDuration)
         {
-            player_agent.gameObject.transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / RotationDuration);
+            player_agent.transform.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime / RotationDuration);
             elapsedTime += Time.deltaTime;
-            yield return null;
+            yield return null; // Continue the rotation over multiple frames
         }
-        player_agent.gameObject.transform.rotation = endRotation;
-
-        // Start moving & narration
-        OnStart_PhotoRoute?.Invoke(this, EventArgs.Empty);
-        GetComponent<NarrationManager>().Narration_After_SprayGame();
-    }
-    private void Handle_OnStartPhotoRoute(object sender, EventArgs e)
-    {
-        StartCoroutine(MoveThroughWaypoints(PhotoRoute_WaypointIndex, PhotoRoute_Waypoints));
+        player_agent.transform.rotation = targetRotation;
     }
     #endregion
-    IEnumerator MoveThroughWaypoints(int index, List<Transform> waypoints)
-    {
-        while (index < waypoints.Count)
-        {
-            player_agent.SetDestination(waypoints[index].position);
-            while (!player_agent.pathPending && player_agent.remainingDistance >= 0.5f)
-            {
-                yield return null;
-            }
-            index++;
-        }
 
+    void OnDestroy()
+    {
+        OnStart_PhotoRoute -= Handle_OnStartPhotoRoute;
+        OnStart_DoorRoute -= Handle_OnStartDoorRoute;
     }
 }
